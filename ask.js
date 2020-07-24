52b592fc3b89335729c099b64854e86df90138dd
Martin Patino <martin@sibipro.com>
2020-07-24 12:28:10 -0700

    fix: remove error catch for better statuscode response

--- ask.js
+++ ask.js
@@ -18,12 +18,6 @@ module.exports = async function ask(url, config = {}) {
   const { responseType, ...init } = { ...defaults, ...encodeBody(config) };
   const response = await fetch(url, init);
 
-  if (!response.ok) {
-    const err = new Error(response.statusText);
-    err.statusCode = response.statusCode;
-    throw err;
-  }
-
   if (responseType === 'json') return response.json();
   if (responseType === 'text') return response.text();
 
